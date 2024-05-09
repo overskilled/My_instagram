ec550cf3432a782d90e57390022ea5ce3b0cac7b
overskilled <ouateedemloic@gmail.com>
2024-05-09 03:11:08 +0100

    Fixed protected routes

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -4,14 +4,6 @@ import { useAuth } from './contexts/AuthContext'
 
 export default function PrivateRoute() {
     const { currentUser } = useAuth()
-    const location = useLocation()
-
-    // Check if the current path is the auth path
-    const isAuthPath = location.pathname === '/auth';
-
-    if (currentUser ) {
-        return isAuthPath ? <Navigate to="/" /> : <Outlet />
-    }
 
     return currentUser ? <Outlet /> : <Navigate to="/auth" />
 }
