ebe6d2038c6f7cd0194ae642b31cec491d68606d
overskilled <ouateedemloic@gmail.com>
2024-05-20 22:09:08 +0100

    Done with the feed post displaying

--- src/components/FeedPosts/FeedPost.jsx
+++ src/components/FeedPosts/FeedPost.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import PostHeader from './PostHeader'
-import PostFooter from './PostFooter'
-import { Box, Image } from '@chakra-ui/react'
+import { Box, Image } from "@chakra-ui/react";
+import PostFooter from "./PostFooter";
+import PostHeader from "./PostHeader";
+import useGetUserProfileById from "../../hooks/useGetUserProfileById";
 
-const FeedPost = ({ img, username, avatar}) => {
-    return (
-        <>
-            <PostHeader username={username} avatar={avatar} />
-                <Box my={2} borderRadius={4} overflow={'hidden'}>
-                    <Image src={img} alt='user profile pic' /> 
-                </Box>
-            <PostFooter username={username} />
-        </>
-    )
-}
+const FeedPost = ({ post }) => {
+	const { userProfile } = useGetUserProfileById(post.createdBy);
 
-export default FeedPost
+	return (
+		<>
+			<PostHeader post={post} creatorProfile={userProfile} />
+			<Box my={2} borderRadius={4} overflow={"hidden"}>
+				<Image src={post.imageURL} alt={"FEED POST IMG"} />
+			</Box>
+			<PostFooter post={post} creatorProfile={userProfile} />
+		</>
+	);
+};
+
+export default FeedPost;
--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,83 +1,94 @@
-import { Box, Button, Flex, Input, InputGroup, InputRightElement, Text } from '@chakra-ui/react'
-import React, { useRef, useState } from 'react'
-import { CommentLogo, NotificationsLogo, UnlikeLogo } from '../../assets/constants'
-import useUserProfileStore from '../../store/userProfileStore'
-import usePostComment from '../../hooks/usePostComment'
-import useLikePost from '../../hooks/useLikePost'
+import { Box, Button, Flex, Input, InputGroup, InputRightElement, Text, useDisclosure } from "@chakra-ui/react";
+import { useRef, useState } from "react";
+import { CommentLogo, NotificationsLogo, UnlikeLogo } from "../../assets/constants";
+import usePostComment from "../../hooks/usePostComment";
+import useAuthStore from "../../store/authStore";
+import useLikePost from "../../hooks/useLikePost";
+import { timeAgo } from "../../utils/timeAgo";
+import CommentsModal from "../Modals/CommentsModal";
 
-const PostFooter = ({ post, username, isProfilePage }) => {
-    const commentRef = useRef()
-    const { userProfile } = useUserProfileStore()
-    const { isCommenting, handlePostComment } = usePostComment()
-    const [comment, setComment] = useState("")
-    const { isLiked, likes, handleLikePost } = useLikePost(post)
+const PostFooter = ({ post, isProfilePage, creatorProfile }) => {
+	const { isCommenting, handlePostComment } = usePostComment();
+	const [comment, setComment] = useState("");
+	const authUser = useAuthStore((state) => state.user);
+	const commentRef = useRef(null);
+	const { handleLikePost, isLiked, likes } = useLikePost(post);
+	const { isOpen, onOpen, onClose } = useDisclosure();
 
-    const handleSubmitComment = async () => {
-        await handlePostComment(post.id, comment)
-        setComment("")
-    }
+	const handleSubmitComment = async () => {
+		await handlePostComment(post.id, comment);
+		setComment("");
+	};
 
+	return (
+		<Box mb={10} marginTop={"auto"}>
+			<Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
+				<Box onClick={handleLikePost} cursor={"pointer"} fontSize={18}>
+					{!isLiked ? <NotificationsLogo /> : <UnlikeLogo />}
+				</Box>
 
-    return (
-        <Box mb={10} mt={"auto"}>
-            <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
-                <Box onClick={handleLikePost} cursor={'pointer'} fontSize={18}>
-                    {!isLiked ? <NotificationsLogo /> : <UnlikeLogo />}
-                </Box>
-                <Box cursor={'pointer'} fontSize={18}
-                    onClick={() => commentRef.current.focus()}
-                >
-                    <CommentLogo />
-                </Box>
-            </Flex>
-            <Text fontWeight={600} fontSize={"sm"}>
-                {likes} Likes
-            </Text>
-            {!isProfilePage && (
-                <>
-                    <Text fontSize={'sm'} fontWeight={700}>
-                        {username}_{" "}
-                        <Text as='span' fontWeight={400}>feeling Good</Text>
-                    </Text>
-                    <Text fontSize={'sm'} color={"gray"}>
-                        View all 1,000 comments
-                    </Text>
-                </>
-            )}
+				<Box cursor={"pointer"} fontSize={18} onClick={() => commentRef.current.focus()}>
+					<CommentLogo />
+				</Box>
+			</Flex>
+			<Text fontWeight={600} fontSize={"sm"}>
+				{likes} likes
+			</Text>
 
-            <Flex
-                alignItems={"center"}
-                gap={2}
-                justifyContent={"space-between"}
-                w={"full"}
-            >
-                <InputGroup>
-                    <Input
-                        variant={'flushed'}
-                        placeholder={"Add a comment..."}
-                        fontSize={14}
-                        value={comment}
-                        onChange={(e) => setComment(e.target.value)}
-                        ref={commentRef}
-                    />
-                    <InputRightElement>
-                        <Button
-                            fontSize={14}
-                            color={"blue.400"}
-                            fontWeight={600}
-                            cursor={'pointer'}
-                            _hover={{ color: "white" }}
-                            bg={'transparent'}
-                            isLoading={isCommenting}
-                            onClick={handleSubmitComment}
-                        >
-                            Post
-                        </Button>
-                    </InputRightElement>
-                </InputGroup>
-            </Flex>
-        </Box>
-    )
-}
+			{isProfilePage && (
+				<Text fontSize='12' color={"gray"}>
+					Posted {timeAgo(post.createdAt)}
+				</Text>
+			)}
 
-export default PostFooter
+			{!isProfilePage && (
+				<>
+					<Text fontSize='sm' fontWeight={700}>
+						{creatorProfile?.username}{" "}
+						<Text as='span' fontWeight={400}>
+							{post.caption}
+						</Text>
+					</Text>
+					{post.comments.length > 0 && (
+						<Text fontSize='sm' color={"gray"} cursor={"pointer"} onClick={onOpen}>
+							View all {post.comments.length} comments
+						</Text>
+					)}
+					{/* COMMENTS MODAL ONLY IN THE HOME PAGE */}
+					{isOpen ? <CommentsModal isOpen={isOpen} onClose={onClose} post={post} /> : null}
+				</>
+			)}
+
+			{authUser && (
+				<Flex alignItems={"center"} gap={2} justifyContent={"space-between"} w={"full"}>
+					<InputGroup>
+						<Input
+							variant={"flushed"}
+							placeholder={"Add a comment..."}
+							fontSize={14}
+							onChange={(e) => setComment(e.target.value)}
+							value={comment}
+							ref={commentRef}
+						/>
+						<InputRightElement>
+							<Button
+								fontSize={14}
+								color={"blue.500"}
+								fontWeight={600}
+								cursor={"pointer"}
+								_hover={{ color: "white" }}
+								bg={"transparent"}
+								onClick={handleSubmitComment}
+								isLoading={isCommenting}
+							>
+								Post
+							</Button>
+						</InputRightElement>
+					</InputGroup>
+				</Flex>
+			)}
+		</Box>
+	);
+};
+
+export default PostFooter;
